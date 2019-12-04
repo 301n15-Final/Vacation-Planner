4db673032b2b9ee9f4a0db9d7e558593c1836b57
Diana Kim <dianajkim@gmail.com>
2019-12-04 12:47:20 -0800

    revised sql schema

--- data/schema.sql
+++ data/schema.sql
@@ -1,8 +1,20 @@
-DROP TABLE IF EXISTS vacation_types, temperature_types, activity_types, packing_items;
+DROP TABLE IF EXISTS user, vacation_types, temperature_types, activity_types, packing_items;
+
+CREATE TABLE user (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255).
+  last_name VARCHAR(255),
+  shorts_temp_lowest INTEGER NOT NULL,
+  fall_temp_low INTEGER NOT NULL,
+  fall_temp_high INTEGER NOT NULL,
+  winter_temp_low INTEGER NOT NULL,
+  winter_temp_high INTEGER NOT NULL
+)
 
 CREATE TABLE vacation_types (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) UNIQUE
+  -- tropics, overnight trips, flights/train, 
 );
 
 CREATE TABLE temperature_types (
@@ -20,11 +32,12 @@ CREATE TABLE activity_types  (
 CREATE TABLE packing_items (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) UNIQUE,
-  vacation_type_id INTEGER NOT NULL,
-  FOREIGN KEY (vacation_type_id) REFERENCES vacation_types(id),
-  temperature_types_id INTEGER NOT NULL,
-  FOREIGN KEY (temperature_types_id) REFERENCES temperature_types(id),
-  activity_types_id INTEGER NOT NULL,
-  FOREIGN KEY (activity_types_id) REFERENCES activity_types(id)
+  vacation_type_ids INTEGER[] NOT NULL,
+  FOREIGN KEY (vacation_type_ids) REFERENCES vacation_types(id),
+  temperature_types_ids INTEGER[] NOT NULL,
+  FOREIGN KEY (temperature_types_ids) REFERENCES temperature_types(id),
+  activity_types_id INTEGER[] NOT NULL,
+  FOREIGN KEY (activity_types_ids) REFERENCES activity_types(id)
 );
 
+INSERT INTO packing_items (name, vacation )
