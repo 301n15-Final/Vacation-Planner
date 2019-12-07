11b08962f9d44dd68d1b7b65a48665ee444763d9
Diana Kim <dianajkim@gmail.com>
2019-12-06 20:23:35 -0800

    Added media query to limit shrinkage of form

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,17 @@
 var summerLowTempSlider = document.getElementById('summer-low');
-var summerOutput = document.getElementById('summer-starts-at');
+var summerOutput = document.getElementById('summer-low');
 summerOutput.innerHTML = summerLowTempSlider.value;
 
 summerLowTempSlider.oninput = function() {
   summerOutput.innerHTML = this.value;
+  console.log('summer low temp: ', this.value);
 };
 
 var winterHighTempSlider = document.getElementById('winter-high');
-var winterOutput = document.getElementById('winter-ends-at');
+var winterOutput = document.getElementById('winter-high');
 winterOutput.innerHTML = winterHighTempSlider.value;
 
 winterHighTempSlider.oninput = function() {
   winterOutput.innerHTML = this.value;
-  console.log('winter temp: ', this.value);
+  console.log('winter high temp: ', this.value);
 };
-
