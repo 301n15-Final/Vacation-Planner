c3c7642675ec6ee32dc4563db671b927cb42cd04
Diana Kim <dianajkim@gmail.com>
2019-12-11 14:34:22 -0800

    start and end date validation

--- public/js/modal.js
+++ public/js/modal.js
@@ -21,25 +21,25 @@ $('#start-date').focusout(() => {
   let validStartDate = compareDate(today, startDate);
   console.log('valid start date? ', validStartDate);
   if(!validStartDate){
-    alert('The start date must be today\'s date or later. \nPlease re-enter your trip\'s start date.');
+    alert('The start date must be today\'s date or later.\nPlease re-enter your trip\'s start date.');
     $('#start-date').val('');
     $('#start-date').focus();
   }
 });
 
+$('#end-date').focusout(() => {
+  let startDate = new Date($('#start-date').val()).toJSON().slice(0, 10);
+  let endDate = new Date($('#end-date').val()).toJSON().slice(0, 10);
+  console.log('startDate: ', endDate);
 
-
-
+  let validEndDate = compareDate(startDate, endDate);
+  if(!validEndDate){
+    alert('The start date must be later than the end date.\nPlease re-enter your trip\'s start and end dates.');
+    $('input[type=date]').val('');
+    $('#start-date').focus();
+  }
+});
 
 function compareDate(date1, date2){
   return date2 > date1;
 }
-
-
-
-
-
-// });
-console.log('today: ', today);
-
-// $('#start-date').on('load', $('#start-date').attr('min', today));
