2e85771c50249e5fa6cf595b6688d6e0d14b9261
Diana Kim <dianajkim@gmail.com>
2019-12-11 14:25:40 -0800

    start date no earlier than today validation

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,26 +1,45 @@
 'use strict';
 
 let $modalEl = $('.modal');
-let today = new Date().toISOString().split('T')[0];
+let today = new Date().toJSON().slice(0, 10);
 
 $('.open-modal').on('click', () => {
   $modalEl.fadeIn(300);
 
   $(window).on('click', (event) => {
-    if($(event.target).is($modalEl)) { 
-      let leaveForm = confirm('Are you sure you want to cancel this form?', 'Yes', 'Cancel');
+    if($(event.target).is($modalEl)){ 
+      let leaveForm = confirm('Are you sure you want to cancel this form?');
       if(leaveForm === true) $modalEl.fadeOut(300);
     }
   });
 });
 
-$('#start-date').on('focusout', checkStartDate);
+$('#start-date').focusout(() => {
+  let startDate = new Date($('#start-date').val()).toJSON().slice(0, 10);
+  console.log('startDate: ', startDate);
 
-function checkStartDate(){
-  
+  let validStartDate = compareDate(today, startDate);
+  console.log('valid start date? ', validStartDate);
+  if(!validStartDate){
+    alert('The start date must be today\'s date or later. \nPlease re-enter your trip\'s start date.');
+    $('#start-date').val('');
+    $('#start-date').focus();
+  }
+});
+
+
+
+
+
+function compareDate(date1, date2){
+  return date2 > date1;
 }
+
+
+
+
 
 // });
 console.log('today: ', today);
 
-$('#start-date').on('load', $('#start-date').attr('min', today));
+// $('#start-date').on('load', $('#start-date').attr('min', today));
