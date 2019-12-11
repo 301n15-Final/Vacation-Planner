9c556b409a0a759e1ebff8e741f4454fdc349ad1
Diana Kim <dianajkim@gmail.com>
2019-12-11 13:08:35 -0800

    accidental click by user outside of form asks for cancellation confirmation

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,10 +1,26 @@
 'use strict';
 
 let $modalEl = $('.modal');
+let today = new Date().toISOString().split('T')[0];
 
 $('.open-modal').on('click', () => {
   $modalEl.fadeIn(300);
+
   $(window).on('click', (event) => {
-    if($(event.target).is($modalEl)) { $modalEl.fadeOut(300); }
+    if($(event.target).is($modalEl)) { 
+      let leaveForm = confirm('Are you sure you want to cancel this form?', 'Yes', 'Cancel');
+      if(leaveForm === true) $modalEl.fadeOut(300);
+    }
   });
 });
+
+$('#start-date').on('focusout', checkStartDate);
+
+function checkStartDate(){
+  
+}
+
+// });
+console.log('today: ', today);
+
+$('#start-date').on('load', $('#start-date').attr('min', today));
